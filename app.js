f589bc0f30f2735e4e07f111e512873329ea386a
troy dorman <troydorman@gmail.com>
2020-12-07 18:00:06 -0600

    initial manager questions added

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const inquirer2 = require("inquirer");
 const path = require("path");
 const fs = require("fs");
 const art = require("./ascii-art-intro");
-let lastTeamMember = false;
+
 
 const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
@@ -16,13 +16,63 @@ const render = require("./lib/htmlRenderer");
 
 clearConsole();
 console.log(art);
-// take manager's info first
+
 
 function clearConsole() {
+
+    // credit : https://gist.github.com/timneutkens/f2933558b8739bbf09104fb27c5c9664
+
     const readline = require('readline')
     const blank = '\n'.repeat(process.stdout.rows)
     console.log(blank)
     readline.cursorTo(process.stdout, 0, 0)
     readline.clearScreenDown(process.stdout)
-}
+};
+
+
+// take manager's info first
+
+async function managerQs() {
+    let manager = await inquirer
+        .prompt([{
+            type: "input",
+            name: "manName",
+            message: "Enter the team manager's name:"
+        },
+        {
+            type: "input",
+            name: "manID",
+            message: "Enter the team manager's id number:"
+        },
+        {
+            type: "input",
+            name: "manEmail",
+            message: "Enter the team manager's email address:"
+        },
+        {
+            type: "input",
+            name: "manOffice",
+            message: "Enter the team manager's office number:"
+        }
+        ]);
+
+    return teamManager = new Manager(manager.manName, manager.manID, manager.manEmail, manager.manOffice);
+
+
+
+};
+
+async function buildTeam() {
+    const finalTeam = [];
+    let bigBoss = await managerQs();
+    finalTeam.push(bigBoss);
+    console.log({finalTeam});
+
+    
+
+
+
+};
+
+buildTeam();
 
