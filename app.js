00020f955e420049b38f5da08cabdcb7cc24d352
troy dorman <troydorman@gmail.com>
2020-12-07 20:18:04 -0600

    got the file writing working. seeing errors in the output HTML.  name didnt make it through rest did. 90% there

--- app.js
+++ app.js
@@ -128,30 +128,26 @@ async function buildTeam() {
     // render HTML file?
     const finalForm = render(finalTeam);
 
-    console.log({finalForm});
+    // check to see if output directory exists
+
+    fs.access(OUTPUT_DIR, error => {
+        if (error) {
+            // create directory
+            fs.mkdirSync(OUTPUT_DIR);
+        }
+    })
+
+    // write HTML file to output directory
+    fs.writeFile(outputPath, finalForm, err => {
+        if (err) throw error;
+        console.log("File written to: " + outputPath)
+    });
+
+
 };
 
 buildTeam();
 
 
 
-// After the user has input all employees desired, call the `render` function (required
-// above) and pass in an array containing all employee objects; the `render` function will
-// generate and return a block of HTML including templated divs for each employee!
-
-// After you have your html, you're now ready to create an HTML file using the HTML
-// returned from the `render` function. Now write it to a file named `team.html` in the
-// `output` folder. You can use the variable `outputPath` above target this location.
-// Hint: you may need to check if the `output` folder exists and create it if it
-// does not.
-
-// HINT: each employee type (manager, engineer, or intern) has slightly different
-// information; write your code to ask different questions via inquirer depending on
-// employee type.
-
-// HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
-// and Intern classes should all extend from a class named Employee; see the directions
-// for further information. Be sure to test out each class and verify it generates an
-// object with the correct structure and methods. This structure will be crucial in order
-// for the provided `render` function to work! ```
 
